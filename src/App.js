74aa8202c2a1a903d760d346f0bcc4f66b3e19e0
die-perez <pzandie@gmail.com>
2021-06-22 10:26:49 -0700

    simple calc functioning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Calculator from './Components/Calculator'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    )
+  }
 }
 
-export default App;
+// export default class Calculator extends Component {
+//     constructor(props) {
+//         super()
+//         this.state = {
+//             total: null
+//         }
+//     }
+
+//     setNum = (e, num) => {
+//       this.setState({ [num]: e.target.value})
+//     }
+
+//     sumNums = (num1, num2) => {
+//       this.setState({
+//         total: parseInt(this.state.num1) + parseInt(this.state.num2)
+//       })
+//     }
+
+//     render (){
+//         return (
+//             <div className="container">
+//             <h1>Add with React!</h1>
+          
+//             <div className="add">
+//             <input type="number"
+//               name="num1"
+//               placeholder="Enter your first number"
+//               value={this.state.num1}
+//               onChange={ (e) => this.setNum(e, 'num1') }
+//             />
+//               <span>+</span>
+//               <input type="number"
+//                 name="num2"
+//                 placeholder="Enter your second number"
+//                 value={this.state.num2}
+//                 onChange={(e) => this.setNum(e, 'num2')}
+//               />
+//               <button onClick={(e) => this.sumNums(e)}>=</button>
+//               <h3>{this.state.total}</h3>
+//             </div>
+//           </div> 
+//         )
+//     }
+
+// }
